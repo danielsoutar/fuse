fa190a4f5d3e3c846d350bdb919c6cf016f09d33
Daniel <danielsoutar144@gmail.com>
2017-11-22 23:28:42 +0000

    fixed - was an issue with non-constant expression. Now printing to test that variables are correctly assigned

--- myfs.c
+++ myfs.c
@@ -524,7 +524,7 @@ void init_fs() {
         }
 
         memcpy(&(current.data), data_id, sizeof(data_id));
-        //parent.data = *data_id;
+        memcpy(&(parent.data), data_id, sizeof(data_id));
 
         memcpy(data_block, &current, sizeof(Dirent));        
         memcpy(ptr_add(data_block, sizeof(Dirent)), &parent, sizeof(Dirent));
@@ -541,6 +541,11 @@ void init_fs() {
 
         if(rc != UNQLITE_OK)
             error_handler(rc);
+
+        printf("Testing some variables before we continue ... \n");
+        printf("Current: \nName: %s\nid is non-zero: %d\n", current.name, uuid_compare(zero_uuid, current.data) != 0);
+        printf("Parent:  \nName: %s\nid is non-zero: %d\n", parent.name, uuid_compare(zero_uuid, parent.data) != 0);
+        
     }
     else {
         if(rc == UNQLITE_OK)
